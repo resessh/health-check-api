8d1e40c8b008209983b10d1a3f8e9f6677371f61
resessh <info@resessh.net>
2020-04-12 15:36:46 +0900

    format

--- src/Application.kt
+++ src/Application.kt
@@ -1,33 +1,35 @@
 package net.resessh
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import com.fasterxml.jackson.databind.*
-import io.ktor.jackson.*
-import io.ktor.features.*
+import io.ktor.application.Application
+import io.ktor.application.install
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.response.respond
+import io.ktor.routing.routing
+import io.ktor.routing.get
+import io.ktor.http.ContentType
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.jackson.jackson
+import io.ktor.features.ContentNegotiation
 
 fun main(args: Array<String>): Unit = io.ktor.server.jetty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-    install(ContentNegotiation) {
-        jackson {
-            enable(SerializationFeature.INDENT_OUTPUT)
-        }
+  install(ContentNegotiation) {
+    jackson {
+      enable(SerializationFeature.INDENT_OUTPUT)
+    }
+  }
+
+  routing {
+    get("/") {
+      call.respondText("Hello, world!", contentType = ContentType.Text.Plain)
     }
 
-    routing {
-        get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
-        }
-
-        get("/json/jackson") {
-            call.respond(mapOf("hello" to "world"))
-        }
+    get("/json/jackson") {
+      call.respond(mapOf("hello" to "world"))
     }
+  }
 }
-
